bd4ed3133c1421a3635b9e0f9338b52e578ec01d
stereoIII6 <stereodocbush@gmail.com>
2023-09-05 05:00:49 +0200

    earthlings

--- src/app.js
+++ src/app.js
@@ -1,7 +1,47 @@
 import "../public/app.scss";
 
-const topics = document.getElementsByClassName("topic");
+const serverside = document.getElementById("serverside");
+const blockchain = document.getElementById("blockchain");
+const backend = document.getElementById("backend");
+const frontend = document.getElementById("frontend");
+const interactive = document.getElementById("interactive");
+const bubble = document.getElementById("speech");
+const bub = document.getElementById("bubble");
 const goBubble = (e) => {
-  console.log(e.target.name);
+  // console.log(e.target.id);
+  switch (e.target.id) {
+    case "serverside":
+      bub.src = "./images/bubble/0.png";
+      bubble.style.display = "block";
+      break;
+    case "blockchain":
+      bub.src = "./images/bubble/1.png";
+      bubble.style.display = "block";
+      break;
+    case "backend":
+      bub.src = "./images/bubble/2.png";
+      bubble.style.display = "block";
+      break;
+    case "frontend":
+      bub.src = "./images/bubble/3.png";
+      bubble.style.display = "block";
+      break;
+    case "interactive":
+      bub.src = "./images/bubble/4.png";
+      bubble.style.display = "block";
+      break;
+  }
 };
-topics.addEventListener("click", goBubble);
+const burst = (e) => {
+  bubble.style.display = "none";
+};
+serverside.addEventListener("mouseover", goBubble);
+blockchain.addEventListener("mouseover", goBubble);
+backend.addEventListener("mouseover", goBubble);
+frontend.addEventListener("mouseover", goBubble);
+interactive.addEventListener("mouseover", goBubble);
+serverside.addEventListener("mouseleave", burst);
+blockchain.addEventListener("mouseleave", burst);
+backend.addEventListener("mouseleave", burst);
+frontend.addEventListener("mouseleave", burst);
+interactive.addEventListener("mouseleave", burst);
