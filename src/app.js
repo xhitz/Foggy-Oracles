014a54a4966e395ee487a6dbe69ca56f9a778b20
stereoIII6 <stereodocbush@gmail.com>
2023-09-05 04:44:21 +0200

    earthling

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
 import "../public/app.scss";
+
+const topics = document.getElementsByClassName("topic");
+const goBubble = (e) => {
+  console.log(e.target.name);
+};
+topics.addEventListener("click", goBubble);
