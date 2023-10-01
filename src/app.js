15325905e2519669906c52c3ab56a61eff17ebc2
stereoIII6 <stereodocbush@gmail.com>
2023-10-01 20:24:51 +0200

    sunny

--- src/app.js
+++ src/app.js
@@ -1,45 +1,51 @@
 import "../polyfills";
+import { loadKeys, storeKeys } from "./helpers.ts";
 import "../public/app.scss";
 import UAuth from "@uauth/js";
 import { Client } from "@xmtp/xmtp-js";
+import { ethers } from "ethers";
+import Resolution from "@unstoppabledomains/resolution";
+const res = "_vrdmtqr44wuho935cv9hg7nuergvnfrv3brwvvkpzonlkzs";
+const resolution = new Resolution({ apiKey: res });
 
-let wallet_address;
+const clientOptions = {
+  env: "production",
+};
 
 const uauth = new UAuth({
   clientID: "7156f6a9-9afe-49e0-a0cc-2edaf1a5aa3b",
   redirectUri: "http://localhost:3000",
   scope: "openid wallet messaging:notifications:optional",
 });
-
-const serverside = document.getElementById("serverside");
-const blockchain = document.getElementById("blockchain");
-const backend = document.getElementById("backend");
-const frontend = document.getElementById("frontend");
-const interactive = document.getElementById("interactive");
-const bubble = document.getElementById("speech");
-const bub = document.getElementById("bubble");
-const headimg = document.getElementById("headimg");
-const abovehead = document.getElementById("abovehead");
-const move = document.getElementById("move");
-const home = document.getElementById("home");
-const up = document.getElementById("btnup");
-const down = document.getElementById("btndown");
 const login = document.getElementById("login");
 const logout = document.getElementById("logout");
 const udchat = document.getElementById("udchat");
+const chat = document.getElementById("chat");
+const list = document.getElementById("convolist");
+const title = document.getElementById("convostitle");
 let UDT = false;
+let signer;
+let provider;
+let xmtp;
 
+const log = async () => {
+  provider = new ethers.providers.Web3Provider(ethereum);
+  await provider.send("eth_accounts", []);
+
+  signer = await provider.getSigner();
+  xmtp = await Client.create(signer, { env: "production" });
+};
 window.login = async () => {
   try {
     const authorization = await uauth.loginWithPopup();
 
-    console.log(authorization.idToken.sub, authorization.idToken.wallet_address, authorization.idToken);
     login.innerHTML = '<img src="./images/udlogo.png" id="udlogo" />' + authorization.idToken.sub;
     login.removeEventListener("click", window.login);
     login.addEventListener("click", toggleUD);
-    let signer = authorization.idToken.wallet_address;
+    const address = authorization.idToken.wallet_address;
 
-    let keys = loadKeys(wallet_address);
+    let keys = loadKeys(address);
+    await log();
     if (!keys) {
       keys = await Client.getKeys(signer, {
         ...clientOptions,
@@ -50,11 +56,14 @@ window.login = async () => {
         // instance
         persistConversations: false,
       });
-      storeKeys(wallet_address, keys);
+      storeKeys(address, keys);
     }
-    const xmtp = await Client.create(wallet_address, { env: "dev" });
-    // Start a conversation with XMTP
-    const conversation = await xmtp.conversations.newConversation(signer);
+    const client = await Client.create(null, {
+      ...clientOptions,
+      privateKeyOverride: keys,
+    });
+    // console.log("sign: ", client);
+    // console.log(authorization.idToken.sub, authorization.idToken.wallet_address, authorization.idToken.eip4361_signature, client);
   } catch (error) {
     console.error(error);
   }
@@ -78,91 +87,61 @@ const toggleUD = (e) => {
     udchat.style.display = "none";
   }
 };
+const getWalletAddr = (domain, ticker) => {
+  resolution
+    .addr(domain, ticker)
+    .then((address) => console.log(`Domain ${domain} has address for ${ticker}: ${address}`))
+    .catch(console.error);
+};
+const reverseTokenId = async (address) => {
+  resolution
+    .reverseTokenId(address)
+    .then((tokenId) => console.log(address, "reversed to", tokenId))
+    // tokenId consists the namehash of the domain with reverse resolution to that address
+    .catch(console.error);
+};
+
+const reverseUrl = async (address) => {
+  resolution
+    .reverse(address, { location: "UNSLayer2" })
+    .then((domain) => console.log(address, "reversed to url", domain))
+    // domain consists of the domain with reverse resolution to that address
+    // use this domain in your application
+    .catch(console.error);
+};
+const msgs = async (convo) => {
+  const messages = await convo.messages();
+  return messages;
+};
+const loadConvos = async () => {
+  const allConversations = await xmtp.conversations.list();
+  console.log(allConversations);
+  allConversations.map(async (convo) => {
+    const messages = await convo.messages();
+    console.log(messages[0]);
+    list.innerHTML += `<div id='${convo.peerAddress}' class="convolistitem"><h2>${convo.peerAddress} - ${messages.length}</h2><i>${messages[messages.length - 1].sent}</i><h3>${messages[messages.length - 1].content}</h3><div class="btn">GO</div></div>`;
+  });
+};
+const newConvo = async (adr) => {
+  const conversation = await xmtp.conversations.newConversation(adr);
+  console.log("convo: ", conversation);
+  // Load all messages in the conversation
+};
+const sendMsg = async (adr, msg) => {
+  const bot = adr;
+  // Start a conversation with XMTP
+  console.log(bot);
+  const isOnNetwork = await Client.canMessage(bot, { env: "production" });
+  console.log(isOnNetwork);
+  // Send a message
+  await conversation.send("[" + message.senderAddress + "] : " + msg);
+  // Listen for new messages in the conversation
+  for await (const message of await conversation.streamMessages()) {
+    console.log(msg);
+    console.log(`[${message.senderAddress}]: ${message.content}`);
+  }
+};
 
 login.addEventListener("click", window.login);
 logout.addEventListener("click", window.logout);
-udchat.addEventListener("click", loadUD);
-
-const goBubble = (e) => {
-  // console.log(e.target.id);
-  switch (e.target.id) {
-    case "serverside":
-      bub.src = "./images/bubble/0.png";
-      bubble.style.display = "block";
-      break;
-    case "blockchain":
-      bub.src = "./images/bubble/1.png";
-      bubble.style.display = "block";
-      break;
-    case "backend":
-      bub.src = "./images/bubble/2.png";
-      bubble.style.display = "block";
-      break;
-    case "frontend":
-      bub.src = "./images/bubble/3.png";
-      bubble.style.display = "block";
-      break;
-    case "interactive":
-      bub.src = "./images/bubble/4.png";
-      bubble.style.display = "block";
-      break;
-  }
-};
-const burst = (e) => {
-  bubble.style.display = "none";
-};
-const paralax = (e) => {
-  // console.log(window.event.clientX, window.innerWidth);
-  let perc = (100 / window.innerWidth) * window.event.clientX * 1.5 - window.innerWidth / 10;
-  let percY = (100 / window.innerHeight) * window.event.clientY * 0.5 + window.innerWidth / 10;
-  let perc2 = ((100 / window.innerWidth) * window.event.clientX) / 2 + window.innerWidth / 10;
-  headimg.style.left = perc + "px";
-  headimg.style.top = -percY + "px";
-  abovehead.style.left = perc2 + "px";
-};
-const goUp = (e) => {
-  up.href = "#home";
-  up.style.display = "none";
-  down.style.display = "block";
-};
-const goDown = (e) => {
-  up.style.display = "block";
-  console.log(window.location.hash.split("#").pop());
-  if (window.location.hash.split("#").pop() == "home" || "") {
-    up.href = "#home";
-    up.style.display = "block";
-    down.href = "#ethos";
-  }
-  if (window.location.hash.split("#").pop() == "ethos") {
-    up.href = "#home";
-    up.style.display = "block";
-    down.href = "#edutainment";
-  }
-  if (window.location.hash.split("#").pop() == "edutainment") {
-    up.style.display = "block";
-    down.href = "#development";
-  }
-  if (window.location.hash.split("#").pop() == "development") {
-    up.style.display = "block";
-    down.href = "#products";
-  }
-  if (window.location.hash.split("#").pop() == "products") {
-    up.style.display = "block";
-    down.style.display = "none";
-    down.href = "#contact";
-  }
-};
-
-serverside.addEventListener("mouseover", goBubble);
-blockchain.addEventListener("mouseover", goBubble);
-backend.addEventListener("mouseover", goBubble);
-frontend.addEventListener("mouseover", goBubble);
-interactive.addEventListener("mouseover", goBubble);
-serverside.addEventListener("mouseleave", burst);
-blockchain.addEventListener("mouseleave", burst);
-backend.addEventListener("mouseleave", burst);
-frontend.addEventListener("mouseleave", burst);
-interactive.addEventListener("mouseleave", burst);
-document.addEventListener("mousemove", paralax);
-up.addEventListener("click", goUp);
-down.addEventListener("click", goDown);
+udchat.addEventListener("click", loadConvos);
